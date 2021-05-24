b34adb3f677fdc0ed6b9ab53c85ca46beff5452d
Bruno Brito <brunopbrito31@hotmail.com>
2021-05-24 00:41:04 -0300

    Projeto iniciado

---
+++ src/main/java/com/example/brunopbrito31/fcrudapi/FcrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.brunopbrito31.fcrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/brunopbrito31/fcrudapi/FcrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brunopbrito31.fcrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
